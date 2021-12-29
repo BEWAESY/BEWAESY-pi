ca84f07a913c7ce4f6a1e99ee48b5838854a71d2
Johannes <johannes.kuerz@gmx.de>
2021-12-29 17:24:26 +0100

    Added activation of pump

--- water.py
+++ water.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime
 import time
+import RPi.GPIO as GPIO
 
 api_url = "http://localhost:3000/"
 with open("config.txt", "r") as f:
@@ -11,26 +12,35 @@ with open("config.txt", "r") as f:
 def checkData(data):
     for value in data[1]:
         # Check if value is for temperature or humidity
-        if value["eventTrigger"] == "time" and value["triggerValue1"] != datetime.now().strftime("%H:%M"):
+        if value["eventTrigger"] == "time" and value["triggerValue1"] == datetime.now().strftime("%H:%M"):
             activatePump(data, value)
+        else:
+            print("No active event")
 
 
-def activatePump(data, value):    
+def activatePump(data, value):
     lastExecution = datetime.strptime(data[0][0]["lastExecution"], "%Y-%m-%dT%H:%M:%S.%fZ")
 
     # Check if plant should be watered
     if (datetime.now() - lastExecution).total_seconds() >= data[0][0]["cooldown"]:
         print(f"Water Plant for {value['seconds']} seconds")
 
-        # Activate pump
-
         # Send data to API that plant was watered
-        data = {
+        apiData = {
             "seconds": value["seconds"],
             "timestamp": datetime.now()
         }
-        responsePost = requests.post(api_url + "systems/watered-plant", params=payload, data=data)
+        responsePost = requests.post(api_url + "systems/watered-plant", params=payload, data=apiData)
         print(responsePost)
+
+        # Activate pump
+        # Start Relay for given time
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(14, GPIO.OUT)
+
+        time.sleep(value["seconds"])
+
+        GPIO.cleanup()
     else:
         print("Cooldown issue!")
 
